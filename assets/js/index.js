46513d61dbd11267991f3dc4a8b08e2eedf46b34
Fernando Contreras Camacho <fcontreras@weecompany.net>
2024-04-21 01:08:07 -0600

    fist commit

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener todas las pestañas y el contenido correspondiente
+    var tabButtons = document.querySelectorAll(".tab-btn");
+    var tabContents = document.querySelectorAll(".tabcontent");
+
+    // Asignar un evento clic a cada pestaña
+    tabButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            // Ocultar todos los contenidos de las pestañas y remover la clase "active" de todos los botones
+            tabContents.forEach(function(content) {
+                content.style.display = "none";
+            });
+            tabButtons.forEach(function(btn) {
+                btn.classList.remove("active");
+            });
+
+            // Mostrar solo el contenido de la pestaña clicada y añadir la clase "active" al botón clicado
+            var tabId = button.getAttribute("data-tab");
+            var tabContent = document.getElementById(tabId);
+            tabContent.style.display = "block";
+            button.classList.add("active");
+        });
+    });
+});
