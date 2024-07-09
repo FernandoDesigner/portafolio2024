3068c55023b46a969df51426c1b6f09854db8b01
Fernando Contreras Camacho <fcontreras@weecompany.net>
2024-07-09 00:15:15 -0600

    dsg tab

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,14 +20,11 @@ const swiper = new Swiper('.swiper', {
     },
   });
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtener todas las pestañas y el contenido correspondiente
     var tabButtons = document.querySelectorAll(".tab-btn");
     var tabContents = document.querySelectorAll(".tabcontent");
 
-    // Asignar un evento clic a cada pestaña
     tabButtons.forEach(function(button) {
         button.addEventListener("click", function() {
-            // Ocultar todos los contenidos de las pestañas y remover la clase "active" de todos los botones
             tabContents.forEach(function(content) {
                 content.style.display = "none";
             });
@@ -35,7 +32,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 btn.classList.remove("active");
             });
 
-            // Mostrar solo el contenido de la pestaña clicada y añadir la clase "active" al botón clicado
             var tabId = button.getAttribute("data-tab");
             var tabContent = document.getElementById(tabId);
             tabContent.style.display = "block";
